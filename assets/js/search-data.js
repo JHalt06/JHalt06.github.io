25dd76d297ef8cf930e099de6f2b95cc51616395
JHalt06 <jamesgeorgehalt@gmail.com>
2025-12-02 22:38:09 +0000

    Deploying to gh-pages from @ JHalt06/JHalt06.github.io@47e1901235a8c40a5c86e16030c6cb94844f5e3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,7 +402,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-symposium-notes",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-symposium-notes",
           title: 'Symposium Notes',
           description: "Imagine never having to take notes again. Symposium Notes is your AI-powered assistant that records, transcribes, and summarizes your lectures, meetings allowing you to pay full attention.",
           section: "Projects",handler: () => {
